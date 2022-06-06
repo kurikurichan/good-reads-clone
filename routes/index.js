78e55aff1edc69844f5f2d7f914b355578924ef9
Patrick McPherson <patrick.j.mcpherson@gmail.com>
2022-06-06 14:12:11 -0700

    login validator

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,25 @@
 var express = require("express");
 var router = express.Router();
 const { csrfProtection, asyncHandler } = require("./utils");
+const { check, validationResult } = require("express-validator");
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
   res.render("index", { title: "a/A Express Skeleton Home" });
 });
+
+const loginValidator = [
+  check("email")
+    .exists({ checkFalsy: true })
+    .withMessage("Must provide email")
+    .isEmail()
+    .withMessage("Please provide a valid email address")
+    .isLength({ max: 62 })
+    .withMessage("Email can't be longer than 62 characters"),
+  check("password")
+    .exists({ checkFalsy: true })
+    .withMessage("please provide a password"),
+];
 
 // login page
 router.get(
