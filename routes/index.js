1a5a8a5e592a94efe0a94411f56b680c19b93fee
Patrick McPherson <patrick.j.mcpherson@gmail.com>
2022-06-06 13:30:08 -0700

    utils

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+const { csrfProtection, asyncHandler } = require("./utils");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'a/A Express Skeleton Home' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "a/A Express Skeleton Home" });
 });
+
+// login page
+router.get(
+  "/login",
+  csrfProtection,
+  asyncHandler(async (req, res, next) => {})
+);
 
 module.exports = router;
