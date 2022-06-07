840052addb60aa61aa6d7ee1ce279065aaf1c607
Alec Venida <alecvenida@gmail.com>
2022-06-07 14:44:46 -0700

    rendered one genre of haunts

--- routes/haunts.js
+++ routes/haunts.js
@@ -9,10 +9,31 @@ const { csrfProtection, asyncHandler } = require('./utils');
 
 /* GET users listing. */
 router.get('/', asyncHandler( async (req, res, next) => {
-  const haunts = await db.Haunt.findAll()
+  const haunts = await db.Haunt.findAll({
+    where: {
+      genreId: 1
+    }
+  })
+  // const haunts2 = await db.Haunt.findAll({
+  //   where: {
+  //     genreId: 2
+  //   }
+  // })
   res.render('haunts', {haunts});
+  // res.render('haunts', {haunts2});
 }));
+// router.get('/', asyncHandler( async (req, res, next) => {
+//   const haunts = await db.Haunt.findAll({
+//     where: {
+//       genreId: 2
+//     }
+//   })
+//   res.render('haunts', {haunts});
+// }));
 
 //  ///haunts/:hauntId(\\d+)  ??   for later on to get ONE haunt
+
+
+//a(href="/haunts/" + haunt.id) click to a specific haunt
 
 module.exports = router;
