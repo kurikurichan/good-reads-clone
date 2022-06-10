d0635492e8ed28bf19fb6afc36d8f57df7896156
Patrick McPherson <patrick.j.mcpherson@gmail.com>
2022-06-10 09:17:26 -0700

    WIP dropdown add to hauntlist

--- public/javascripts/haunt.js
+++ public/javascripts/haunt.js
@@ -1,5 +1,20 @@
 document.addEventListener("DOMContentLoaded", e => {
-  const addHauntToHauntlistButton = document.querySelector("#addToHauntlist");
+  // const hauntlistsButton = document.querySelector("#hauntlistsButton");
+  const hauntlists = document.querySelectorAll(".hauntlist");
+  const addToHauntlist = document.querySelector("#addToHauntlist *");
+  // const notAddToHauntlist = document.querySelector(":not(#addToHauntlist)");
 
-  addHauntToHauntlistButton.addEventListener("click", e => {});
+  addToHauntlist.classList.add("test");
+  console.log("hauntlists", hauntlists);
+  addToHauntlist.addEventListener("focus", e => {
+    hauntlists.forEach(hauntlist => {
+      hauntlist.classList.remove("hide");
+    });
+  });
+  addToHauntlist.addEventListener("focusout", e => {
+    if (document.activeElement.classList.contains("focusDropDown")) return;
+    hauntlists.forEach(hauntlist => {
+      hauntlist.classList.add("hide");
+    });
+  });
 });
