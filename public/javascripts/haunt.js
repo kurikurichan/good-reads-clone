ba3ca954abbb9421efcb4ed8d79579cb2c49d12e
Krista Strucke <kstrucke@gmail.com>
2022-06-10 10:22:17 -0700

    troubleshooting dropdown

--- public/javascripts/haunt.js
+++ public/javascripts/haunt.js
@@ -1,20 +1,42 @@
-document.addEventListener("DOMContentLoaded", e => {
-  // const hauntlistsButton = document.querySelector("#hauntlistsButton");
-  const hauntlists = document.querySelectorAll(".hauntlist");
-  const addToHauntlist = document.querySelector("#addToHauntlist *");
-  // const notAddToHauntlist = document.querySelector(":not(#addToHauntlist)");
+// Script to hide and show dropdown for adding haunts to hauntlist on haunts/:id page
 
-  addToHauntlist.classList.add("test");
-  console.log("hauntlists", hauntlists);
-  addToHauntlist.addEventListener("focus", e => {
-    hauntlists.forEach(hauntlist => {
-      hauntlist.classList.remove("hide");
+document.addEventListener("DOMContentLoaded", e => {
+  const hauntlists = document.querySelectorAll(".hauntlist"); // children li's of add haunt list button
+  const addToHauntlist = document.querySelectorAll("#addToHauntlist *"); // "Add to Haunt List" button + its children buttons (inside divs)
+
+  document.addEventListener("click", e => {
+    console.log(e.target);
+  });
+
+  // iterate through user's hauntlists & hide & show buttons
+  addToHauntlist.forEach(hauntListEle => {
+
+    // show buttons
+    hauntListEle.addEventListener("focus", e => {
+      hauntlists.forEach(hauntlist => {
+        hauntlist.classList.remove("hide");
+      });
+    });
+
+    // hide buttons
+    hauntListEle.addEventListener("focusout", e => {
+      hauntlists.forEach(hauntlist => {
+        hauntlist.classList.add("hide");
+      });
     });
   });
-  addToHauntlist.addEventListener("focusout", e => {
-    if (document.activeElement.classList.contains("focusDropDown")) return;
-    hauntlists.forEach(hauntlist => {
-      hauntlist.classList.add("hide");
+
+  // iterate through children divs
+  hauntlists.forEach(dive => {
+
+    // add action to button click
+    dive.addEventListener("click", e => {
+      e.stopPropagation();
+      console.log("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+      console.log(e.target);
     });
   });
 });
+
+
+// TODO: create action of clicking buttons add haunt to hauntlists
