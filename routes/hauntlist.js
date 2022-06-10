bf3dafa9c6d3f7e51de4250a56652d315f6ee9f3
Krista Strucke <kstrucke@gmail.com>
2022-06-10 13:51:32 -0700

    added a post route and a fetch request suggestion for adding hauntlists

--- routes/hauntlist.js
+++ routes/hauntlist.js
@@ -5,6 +5,7 @@ const db = require("../db/models");
 
 const { csrfProtection, asyncHandler } = require("./utils");
 
+// grab a hauntlist???
 router.get(
   "/:id(\\d+)",
   asyncHandler(async (req, res, next) => {
@@ -22,6 +23,7 @@ router.get(
   })
 );
 
+// update a hauntlist by deleting a haunt from it
 router.patch(
   "/:id(\\d+)",
   asyncHandler(async (req, res, next) => {
@@ -35,5 +37,45 @@ router.patch(
     console.log("destroyed");
   })
 );
+
+// add a haunt to a hauntlist (this is made for the haunts/:id page)
+// user is clicking a button that sends the request for each time they want to add haunt to hauntlist
+router.post(
+  "/:id(\\d+)",
+  csrfProtection, // remind Patrick to add a csrfToken to his haunt status thing? idk if needed for a button tbh
+  asyncHandler(async (req, res, next) => {
+    const hauntListId = req.params.id;
+    const hauntId = req.body.hauntId; // OR retrieve button id/option id via dom selector
+    console.log(hauntListId);ß
+    console.log(hauntId);
+
+    const hauntJoinList = await db.HauntJoinList.findOne({
+      where: { hauntListId, hauntId },
+    });
+
+    // if the input is valid, let's create a new object & save it
+    if (hauntJoinList) {
+      await db.HauntJoinList.create({
+        hauntListId,
+        hauntId
+      });
+      res.end();
+    }
+    // else must build an error
+}));
+
+// fetch request to add to front end for each button click to add a haunt to the haunt list
+/*
+(async function() {
+  const addToHauntList = await fetch(`/hauntlists/${hauntListId}`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(data)
+  });
+  return addToHauntList.json();
+});
+*/
 
 module.exports = router;
