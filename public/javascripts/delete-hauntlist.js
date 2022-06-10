b6709adb80a4eb0c567c8a7f8698079671ec9160
Alec Venida <alecvenida@gmail.com>
2022-06-10 14:23:58 -0700

    front end is ok, getting 500

--- public/javascripts/delete-hauntlist.js
+++ public/javascripts/delete-hauntlist.js
@@ -1,3 +1,4 @@
+
 document.addEventListener("DOMContentLoaded", e => {
 let deleteMode = false
 
@@ -20,46 +21,86 @@ const removeDelete = () => {
   deleteButton.innerText = "Delete a Hauntlist"
 }
 
-let checkBoxes = document.querySelector(".hide");
+// let checkBoxes = document.querySelector(".hide");  //do we need this?
 
 //toggle delete mode
-deleteButton.addEventListener("click", event => {
-  deleteMode ? (deleteMode = false) : (deleteMode = true)
+  deleteButton.addEventListener("click", event => {
+    deleteMode ? (deleteMode = false) : (deleteMode = true)
 
-  if (deleteMode) {
-    //alert("Select lists to delete")
-    document.getElementsByTagName("h2")[0].innerText += "\n(Select lists to delete)";
-
-
-    deleteButton.innerText = "Confirm" //delete becomes save
-
-    //adds cancel button in delete mode
-    const deleteButtonsDiv = document.querySelector("#deleteButtons");
-      deleteButtonsDiv.append(cancelButton);
-
-    // grabs each hauntlist and appends checkboxes to them
-    const checkBoxLis = document.querySelectorAll(".checkBoxes-line li");
-    checkBoxLis.forEach(checkBoxLi =>{
-      const seeCheckBox = document.createElement('input');
-      seeCheckBox.type = "checkbox"
-      seeCheckBox.classList = "selectedForDel";
-      seeCheckBox.setAttribute("id", checkBoxLi.getAttribute("id") + "-checkbox");
-      checkBoxLi.append(seeCheckBox);
-
-    });
-  }
-
-  // if(!deleteMode){
-
-  //   if (seeCheckBox.checked)
+    if (deleteMode) {
+      //alert("Select lists to delete")
+      document.getElementsByTagName("h2")[0].innerText += "\n(Select lists to delete)";
 
 
-  //   const checked = document.querySelectorAll()
-  // }
+      deleteButton.innerText = "Confirm" //delete becomes save
 
-})
+      //adds cancel button in delete mode
+      const deleteButtonsDiv = document.querySelector("#deleteButtons");
+        deleteButtonsDiv.append(cancelButton);
+
+      // grabs each hauntlist and appends checkboxes to them
+      const checkBoxLis = document.querySelectorAll(".checkBoxes-line li");
+      checkBoxLis.forEach(checkBoxLi =>{
+        const seeCheckBox = document.createElement('input');
+        seeCheckBox.type = "checkbox"
+        seeCheckBox.classList = "preChecked";
+        seeCheckBox.setAttribute("id", checkBoxLi.getAttribute("id") + "-checkbox");  // this is coming up as null -checkbox
+        checkBoxLi.append(seeCheckBox);
+
+      });
+    }
+
+    const getUserId = async () => {
+      const userURL = await document.URL.split("/");
+      return userURL[userURL.length - 1];
+    };
+
+    if(!deleteMode){
+
+      const allCheckBoxes = document.querySelectorAll(".preChecked")
+      // console.log("ALLL THE BOXXXESSSSSSSS", allCheckBoxes)
+      allCheckBoxes.forEach(checkBox => {
+          // console.log("This is a node of all thecheckboxes:", checkBox)
+
+        if (checkBox.checked) {
+          // console.log("YO, I'M TOTALLY CHECKED RN")
+
+          const deleteHauntList = async () => {
+            const userId = await getUserId();
+            const hauntListId = checkBox.id.split("-")[0]
+            console.log("THIS IS THE HAUNTLIST ID:", hauntListId);
+            // const userId =
+            const res = await fetch("/hauntlists/" + hauntListId, {
+              method: "PATCH",
+              headers: { "Content-Type": "application/json" },
+              body: JSON.stringify({ hauntListId }),
+            })
+          };
+          deleteHauntList();
+        }
+      });
+      removeDelete();
+    }
+
+  })
+
+
+    // if checked, give classname of '.checked"'
+
+    // make variable of all checked via querySlectorAll
+
+    // for each this variable
+
+    // make async func
+
+
+    // outside async func, call async func
+
+    // outside forEach, call removeDelete
 
 
 
 
-})
+
+
+ })
